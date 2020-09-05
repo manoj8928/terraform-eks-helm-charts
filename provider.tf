56c18c2e9267aab3656bda29ec78483295ecb0e7
manoj8928 <manoj.bhagwat60@gmail.com>
2020-09-05 19:59:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,16 @@
-# we provision resources only in aws region which is defined by the variables
-provider "aws" {
-  region = "eu-central-1"
+provider "helm" {
+  kubernetes {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.default.token
+  load_config_file       = false
+  }
 }
 
-terraform {
-  backend "s3" {}
+data "aws_eks_cluster" "default" {
+  name = "DEMO-CLUSTER"
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = "DEMO-CLUSTER"
 }
